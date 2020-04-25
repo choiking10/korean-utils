f39b1b2c529391209bf63bdcc836bbdf4495f62e
Yunho Choi <choiking10@kaist.ac.kr>
2020-04-25 10:16:33 +0900

    Add exception

--- count_nouns.py
+++ count_nouns.py
@@ -7,17 +7,20 @@ from konlpy.tag import Okt
 nlpy = Okt()
 
 for filename in os.listdir("test"):
-    with open(os.path.join("test", filename), 'r', encoding="utf-8") as f:  # open in readonly mode
-        print("file :" + filename)
-        sentences = f.read()
-        nouns = nlpy.nouns(sentences)
-        counter = {}
-        for noun in nouns:
-            if noun not in counter:
-                counter[noun] = 1
-            else:
-                counter[noun] += 1
-        sorter = [(value, key) for key, value in counter.items()]
-        sorter.sort()
-        sorter.reverse()
-        pprint(sorter)
+    try:
+        with open(os.path.join("test", filename), 'r', encoding="utf-8") as f:  # open in readonly mode
+            print("file :" + filename)
+            sentences = f.read()
+            nouns = nlpy.nouns(sentences)
+            counter = {}
+            for noun in nouns:
+                if noun not in counter:
+                    counter[noun] = 1
+                else:
+                    counter[noun] += 1
+            sorter = [(value, key) for key, value in counter.items()]
+            sorter.sort()
+            sorter.reverse()
+            pprint(sorter)
+    except Exception as e:
+        print(filename + " cannot open\n\n")
